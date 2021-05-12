52f65b3459aff7f4035b540c7e2013452bc3c398
snoogft <snoo@gft.com>
2021-05-12 14:10:07 +0200

    Uncommented bastion_host and commented out gke

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -16,28 +16,28 @@ module "vpc_network" {
   subnet-01-services-ip       = var.subnet-01-services-ip
 }
 
-//module "bastion_host" {
-//  source     = "../../modules/iap-tunneling"
-//  members    = var.members
-//  project    = var.project
-//  region     = var.region
-//  zone       = var.zone
-//  network    = module.vpc_network.network_name
-//  subnetwork = local.subnet-01_name
-//  depends_on = [module.vpc_network]
-//}
-
-module "gke" {
-  source                         = "../../modules/cluster"
-  project_id                     = var.project
-  region                         = var.region
-  zones                          = [var.zone]
-  environment                    = local.env
-  network                        = module.vpc_network.network_name
-  subnetwork                     = local.subnet-01_name
-  ip_cidr_range                  = var.subnet-01_ip
-  ip_range_pods_name             = var.subnet-01-secondary-01_name
-  ip_range_services_name         = var.subnet-01-services-name
-  compute_engine_service_account = var.compute_engine_service_account
-  depends_on                     = [module.vpc_network]
+module "bastion_host" {
+  source     = "../../modules/iap-tunneling"
+  members    = var.members
+  project    = var.project
+  region     = var.region
+  zone       = var.zone
+  network    = module.vpc_network.network_name
+  subnetwork = local.subnet-01_name
+  depends_on = [module.vpc_network]
 }
+
+# module "gke" {
+#   source                         = "../../modules/cluster"
+#   project_id                     = var.project
+#   region                         = var.region
+#   zones                          = [var.zone]
+#   environment                    = local.env
+#   network                        = module.vpc_network.network_name
+#   subnetwork                     = local.subnet-01_name
+#   ip_cidr_range                  = var.subnet-01_ip
+#   ip_range_pods_name             = var.subnet-01-secondary-01_name
+#   ip_range_services_name         = var.subnet-01-services-name
+#   compute_engine_service_account = var.compute_engine_service_account
+#   depends_on                     = [module.vpc_network]
+# }
