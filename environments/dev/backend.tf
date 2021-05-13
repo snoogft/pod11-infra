07c12263caacaf92cd440fbd89376c622d11875c
snoogft <snoo@gft.com>
2021-05-13 12:39:19 +0200

    Run terraform fmt -recursive

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "gcs" {
-    bucket = "pol-pod11-dev-01-tfstate"
-    prefix = "dev"
+    bucket      = "pol-pod11-dev-01-tfstate"
+    prefix      = "dev"
     credentials = "pol-pod11-dev-01.json"
   }
 }
