47874cdc709ee44abe63ba9a6030b0fb23dbe2dc
snoogft <snoo@gft.com>
2021-05-20 11:48:35 +0200

    Added default_max_pods_per_node

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -1,19 +1,20 @@
 module "gke" {
-  source                  = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
-  project_id              = var.project_id
-  name                    = "${var.environment}-cluster"
-  regional                = false
-  region                  = var.region
-  zones                   = var.zones
-  network                 = var.network
-  subnetwork              = var.subnetwork
-  ip_range_pods           = var.ip_range_pods_name
-  ip_range_services       = var.ip_range_services_name
-  create_service_account  = false
-  service_account         = var.compute_engine_service_account
-  enable_private_endpoint = true
-  enable_private_nodes    = true
-  master_ipv4_cidr_block  = "172.16.0.0/28"
+  source                    = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
+  project_id                = var.project_id
+  name                      = "${var.environment}-cluster"
+  regional                  = false
+  region                    = var.region
+  zones                     = var.zones
+  network                   = var.network
+  subnetwork                = var.subnetwork
+  ip_range_pods             = var.ip_range_pods_name
+  ip_range_services         = var.ip_range_services_name
+  create_service_account    = false
+  service_account           = var.compute_engine_service_account
+  enable_private_endpoint   = true
+  enable_private_nodes      = true
+  default_max_pods_per_node = 55
+  master_ipv4_cidr_block    = "172.16.0.0/28"
 
   master_authorized_networks = [
     {
