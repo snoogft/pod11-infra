7a81b0fed79deab2ca8931a718a37a15deb0c2e9
mzkz <mzkz@gft.com>
2021-05-31 15:03:33 +0200

    Revert "Merge pull request #139 from snoogft/master"
    This reverts commit e906fe94b5d2a8fbb058cb3cee2416e5ea94dff2, reversing
    changes made to 93d54449e9384cde278e7105d3730d75dd9bec8b.

--- environments/dev-secondary/k8s-config/main.tf
+++
@@ -1,7 +0,0 @@
-module "workload_identity" {
-  source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
-  project_id          = var.project
-  name                = "iden-${var.cluster_name}"
-  namespace           = "default"
-  use_existing_k8s_sa = false
-}
