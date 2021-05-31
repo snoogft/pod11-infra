be876490fa1c94d6e73f743432bbb8a87f563e50
mzkz <mzkz@gft.com>
2021-05-31 14:19:13 +0200

    Configure second env using terraform workspaces

--- environments/dev/k8s-config/run.sh
+++ environments/dev/k8s-config/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -xe
 
+terraform workspace new ${BRANCH_NAME}
 terraform init -upgrade
 terraform validate
-terraform plan
-terraform apply -auto-approve
+terraform plan -var-file=${BRANCH_NAME}.tfvars
+terraform apply -auto-approve -var-file=${BRANCH_NAME}.tfvars
