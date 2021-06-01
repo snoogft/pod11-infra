8e92c31323fb8161b18f8618c90a0b38f0c9c721
mzkz <mzkz@gft.com>
2021-06-01 12:14:05 +0200

    Terraform fmt

--- environments/dev/k8s-config/providers.tf
+++ environments/dev/k8s-config/providers.tf
@@ -14,8 +14,8 @@ data "google_container_cluster" "my_cluster" {
 }
 
 provider "kubernetes" {
-  host             = "https://${data.google_container_cluster.my_cluster.endpoint}"
-  token            = data.google_client_config.default.access_token
+  host  = "https://${data.google_container_cluster.my_cluster.endpoint}"
+  token = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(
     data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate,
   )
