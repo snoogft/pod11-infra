b4ab84c5676ceaa13557c2cd449e9fff29dc8a42
snoogft <snoo@gft.com>
2021-06-01 18:22:57 +0200

    Added k8s-config dir

---
+++ environments/dev-secondary/k8s-config/providers.tf
@@ -0,0 +1,22 @@
+provider "google" {
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+data "google_client_config" "default" {
+
+}
+
+data "google_container_cluster" "my_cluster" {
+  name     = var.cluster_name
+  location = var.zone
+}
+
+provider "kubernetes" {
+  host  = "https://${data.google_container_cluster.my_cluster.endpoint}"
+  token = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(
+    data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate,
+  )
+}
