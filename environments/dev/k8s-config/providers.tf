85bada2688d3b831c2932e588969f6b13f4a8f20
snoogft <snoo@gft.com>
2021-06-04 10:43:30 +0200

    Calling kubeconfig from gcloud command

--- environments/dev/k8s-config/providers.tf
+++ environments/dev/k8s-config/providers.tf
@@ -23,9 +23,5 @@ data "google_container_cluster" "my_cluster" {
 }
 
 provider "kubernetes" {
-  host  = "https://${data.google_container_cluster.my_cluster.endpoint}"
-  token = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(
-    data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate,
-  )
+  config_path = "~/.kube/config"
 }
