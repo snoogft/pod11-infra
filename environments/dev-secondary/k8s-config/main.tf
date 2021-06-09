be35f203f81fff92ee8942b07569d9686ab09927
Wyrzutowicz <jwwz@gft.com>
2021-06-09 17:52:03 +0200

    returning to previous setup- creting new workload identity

--- environments/dev-secondary/k8s-config/main.tf
+++ environments/dev-secondary/k8s-config/main.tf
@@ -1,16 +1,29 @@
-resource "kubernetes_service_account" "preexisting" {
-  metadata {
-    name = "cluster-sa"
-    namespace = "default"
-  }
-}
+# resource "kubernetes_service_account" "preexisting" {
+#   metadata {
+#     name = "cluster-sa"
+#     namespace = "default"
+#   }
+# }
+
+# module "workload_identity" {
+#   source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
+#   project_id          = var.project
+#   name                = "cluster-sa"
+#   namespace           = "default"
+#   use_existing_k8s_sa = true
+#   roles = [
+#     "roles/cloudtrace.agent",
+#     "roles/monitoring.metricWriter",
+#     "roles/cloudsql.client",
+#   ]
+# }
 
 module "workload_identity" {
   source              = "terraform-google-modules/kubernetes-engine/google//modules/workload-identity"
   project_id          = var.project
-  name                = "cluster-sa"
+  name                = "iden-${var.cluster_name}"
   namespace           = "default"
-  use_existing_k8s_sa = true
+  use_existing_k8s_sa = false
   roles = [
     "roles/cloudtrace.agent",
     "roles/monitoring.metricWriter",
