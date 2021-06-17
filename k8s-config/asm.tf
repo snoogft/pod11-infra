3f986a9986c98c7b3cc8815e1f69e38a07d79f31
PaulaDurasikGFT <paula.durasik@gft.com>
2021-06-17 15:15:54 +0200

    enable cluster labels

--- k8s-config/asm.tf
+++ k8s-config/asm.tf
@@ -1,8 +1,9 @@
 module "asm-primary" {
-  source           = "terraform-google-modules/kubernetes-engine/google//modules/asm"
-  version          = "15.0.0"
-  project_id       = var.project
-  cluster_name     = data.terraform_remote_state.workspaces.outputs.gke_cluster_name
-  location         = data.terraform_remote_state.workspaces.outputs.gke_location
-  cluster_endpoint = data.terraform_remote_state.workspaces.outputs.gke_cluster_endpoint
+  source                = "terraform-google-modules/kubernetes-engine/google//modules/asm"
+  version               = "15.0.0"
+  project_id            = var.project
+  cluster_name          = data.terraform_remote_state.workspaces.outputs.gke_cluster_name
+  location              = data.terraform_remote_state.workspaces.outputs.gke_location
+  enable_cluster_labels = true
+  cluster_endpoint      = data.terraform_remote_state.workspaces.outputs.gke_cluster_endpoint
 }
