16f7d480377d54a603073c31402825f7b6bca18d
Wyrzutowicz <jwwz@gft.com>
2021-06-18 13:35:01 +0200

    removed reference to vpc_2

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -3,19 +3,9 @@ output "network" {
   description = "The VPC resource being created"
 }
 
-output "network_2" {
-  value       = module.vpc_2.network
-  description = "The VPC_2 resource being created"
-}
-
 output "network_name" {
   value       = module.vpc.network_name
   description = "The name of the VPC being created"
-}
-
-output "network_2_name" {
-  value       = module.vpc_2.network_name
-  description = "The name of the VPC_2 being created"
 }
 
 output "subnets" {
@@ -23,17 +13,7 @@ output "subnets" {
   description = "The created subnet resources"
 }
 
-output "subnets_vpc_2" {
-  value       = module.subnets_vpc_2.subnets
-  description = "The created subnet resources"
-}
-
 output "network_self_link" {
   value       = module.vpc.network_self_link
-  description = "The URI of the VPC being created"
-}
-
-output "network_self_link_vpc_2" {
-  value       = module.vpc_2.network_self_link
   description = "The URI of the VPC being created"
 }
