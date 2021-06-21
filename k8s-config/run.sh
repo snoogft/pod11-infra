6d6105ded73f0506994c5d4157d7113bafcffd8a
Wyrzutowicz <jwwz@gft.com>
2021-06-21 18:00:17 +0200

    removed terraform workspace new dev command from k8s-config/run.sh

--- k8s-config/run.sh
+++ k8s-config/run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -xe
 
 terraform init -upgrade
-terraform workspace new ${BRANCH_NAME}
 terraform workspace select ${BRANCH_NAME}
 terraform plan -var-file=${BRANCH_NAME}.tfvars
 terraform apply -var-file=${BRANCH_NAME}.tfvars -auto-approve
