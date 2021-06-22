c31b8e3eac3db9cb75d2bc0091237faedebdda72
mzkz <mzkz@gft.com>
2021-06-22 14:40:48 +0200

    Set label namespace default istio.io/rev=asm-195-2

--- k8s-config/asm.tf
+++ k8s-config/asm.tf
@@ -7,3 +7,13 @@ module "asm-primary" {
   enable_cluster_labels = true
   cluster_endpoint      = data.terraform_remote_state.workspaces.outputs.gke_cluster_endpoint
 }
+
+module "kubectl" {
+  source = "terraform-google-modules/gcloud/google//modules/kubectl-wrapper"
+
+  project_id              = var.project
+  cluster_name            = data.terraform_remote_state.workspaces.outputs.gke_cluster_name
+  cluster_location        = data.terraform_remote_state.workspaces.outputs.gke_location
+  kubectl_create_command  = "kubectl label namespace default istio.io/rev=asm-195-2 --overwrite"
+  kubectl_destroy_command = "kubectl label namespaces default istio.io/rev-"
+}
