33f90b40b68175bc156f65c30f05f4e22d2748b7
Wyrzutowicz <jwwz@gft.com>
2021-06-22 17:38:18 +0200

    fix for name of data resorce in bastion madule

--- modules/bastion-host/main.tf
+++ modules/bastion-host/main.tf
@@ -14,7 +14,7 @@ module "instance_template" {
   }
 }
 
-data "template_file" "bastion_host_startup_script" {
+data "template_file" "bastion_host_startup_script_1" {
   template = file("${path.module}/scripts/startup_script_bastion_host.tpl")
   vars = {
     cluster_name = "${var.env}-cluster-${var.cluster_number}"
@@ -34,7 +34,7 @@ resource "google_compute_instance_from_template" "vm" {
   name                    = var.instance
   project                 = var.project
   zone                    = var.zone
-  metadata_startup_script = var.startup_script_for_bastion  //data.template_file.bastion_host_startup_script.rendered
+  metadata_startup_script = data.template_file.bastion_host_startup_script_${var.cluster_number}.rendered
   network_interface {
     subnetwork = var.subnetwork
   }
--- modules/bastion-host/variables.tf
+++ modules/bastion-host/variables.tf
@@ -56,4 +56,5 @@ variable "fw_name_allow_ssh_from_iap" {
 
 variable "metadata_startup_script" {
   description = "Path to startup script template for bastion host 1 or bastion host 2"
+  default = "data.template_file.bastion_host_startup_script.rendered"
 }
