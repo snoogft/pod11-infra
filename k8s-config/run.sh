390fafc4947a0a52c6455ef538d76ce64c1c44cb
snoogft <snoo@gft.com>
2021-06-23 17:50:27 +0200

    Added export to run.sh

--- k8s-config/run.sh
+++ k8s-config/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 
-TF_VAR_cluster_name=$1 #first argument in call for run.sh in cloudbuild-dev.yaml
-TF_VAR_zone=$2 #second argument in call for run.sh in cloudbuild-dev.yaml
+export TF_VAR_cluster_name=$1 #first argument in call for run.sh in cloudbuild-dev.yaml
+export TF_VAR_zone=$2 #second argument in call for run.sh in cloudbuild-dev.yaml
 
 terraform init -upgrade
 terraform workspace select ${TF_VAR_cluster_name}
