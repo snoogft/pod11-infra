3ef2fb397d91d109f522fcd0545b8e179e653c40
Wyrzutowicz <jwwz@gft.com>
2021-06-23 09:51:59 +0200

    added separate module for bastion host for cluster 2

--- modules/bastion-host/main.tf
+++ modules/bastion-host/main.tf
@@ -22,19 +22,19 @@ data "template_file" "bastion_host_startup_script_1" {
   }
 }
 
-data "template_file" "bastion_host_startup_script_2" {
-  template = file("${path.module}/scripts/startup_script_bastion_host.tpl")
-  vars = {
-    cluster_name = "${var.env}-cluster-${var.cluster_number}"
-    zone         = var.zone
-  }
-}
+# data "template_file" "bastion_host_startup_script_2" {
+#   template = file("${path.module}/scripts/startup_script_bastion_host.tpl")
+#   vars = {
+#     cluster_name = "${var.env}-cluster-${var.cluster_number}"
+#     zone         = var.zone
+#   }
+# }
 
 resource "google_compute_instance_from_template" "vm" {
   name                    = var.instance
   project                 = var.project
   zone                    = var.zone
-  metadata_startup_script = data.template_file.bastion_host_startup_script_"${var.cluster_number}".rendered
+  metadata_startup_script = data.template_file.bastion_host_startup_script_1.rendered
   network_interface {
     subnetwork = var.subnetwork
   }
