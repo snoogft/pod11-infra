a38e3aae9a47bc110fcd7bfd737057b2dbce2755
snoogft <snoo@gft.com>
2021-06-23 18:10:44 +0200

    Trying to export in cloudbuild-dev.yaml

--- k8s-config/run.sh
+++ k8s-config/run.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -xe
 
-export TF_VAR_cluster_name=$1 #first argument in call for run.sh in cloudbuild-dev.yaml
-export TF_VAR_zone=$2 #second argument in call for run.sh in cloudbuild-dev.yaml
-
 terraform init -upgrade
 terraform workspace select ${TF_VAR_cluster_name}
 terraform plan -var-file=${BRANCH_NAME}.tfvars
