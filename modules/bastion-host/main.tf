aab401062eae6af6bbd77478393c0bf461707cff
katmar-creator <katarzyna.s.marciniak@gmail.com>
2021-06-23 12:25:23 +0200

    delete unused bastion_claster_2 folder

--- modules/bastion-host/main.tf
+++ modules/bastion-host/main.tf
@@ -16,7 +16,7 @@ module "instance_template" {
   }
 }
 
-data "template_file" "bastion_host_startup_script_1" {
+data "template_file" "bastion_host_startup_script" {
   template = file("${path.module}/scripts/startup_script_bastion_host.tpl")
   vars = {
     cluster_name = "${var.env}-cluster-${var.cluster_number}"
@@ -24,35 +24,16 @@ data "template_file" "bastion_host_startup_script_1" {
   }
 }
 
-# data "template_file" "bastion_host_startup_script_2" {
-#   template = file("${path.module}/scripts/startup_script_bastion_host.tpl")
-#   vars = {
-#     cluster_name = "${var.env}-cluster-${var.cluster_number}"
-#     zone         = var.zone
-#   }
-# }
-
 resource "google_compute_instance_from_template" "vm" {
   name                    = var.instance
   project                 = var.project
   zone                    = var.zone
-  metadata_startup_script = data.template_file.bastion_host_startup_script_1.rendered
+  metadata_startup_script = data.template_file.bastion_host_startup_script.rendered
   network_interface {
     subnetwork = var.subnetwork
   }
   source_instance_template = module.instance_template.self_link
 }
-
-# resource "google_compute_instance_from_template_2" "vm_2" {
-#   name                    = var.instance
-#   project                 = var.project
-#   zone                    = var.zone
-#   metadata_startup_script = data.template_file.bastion_host_startup_script_2.rendered
-#   network_interface {
-#     subnetwork = var.subnetwork
-#   }
-#   source_instance_template = module.instance_template.self_link
-# }
 
 module "iap_tunneling" {
   source                     = "terraform-google-modules/bastion-host/google//modules/iap-tunneling"
