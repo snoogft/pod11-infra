dcee96dc0ca473ce011779b94d51efc6612692a3
snoogft <snoo@gft.com>
2021-06-23 18:44:18 +0200

    Fixed destroy.sh

--- k8s-config/destroy.sh
+++ k8s-config/destroy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -xe
 
-TF_VAR_cluster_name=$1 #first argument in call for run.sh in cloudbuild-dev.yaml
-TF_VAR_zone=$2 #second argument in call for run.sh in cloudbuild-dev.yaml
+export TF_VAR_cluster_name=$1 #first argument in call for run.sh in cloudbuild-dev.yaml
+export TF_VAR_zone=$2 #second argument in call for run.sh in cloudbuild-dev.yaml
 
 terraform init -upgrade
-terraform workspace select ${BRANCH_NAME}
+terraform workspace select ${TF_VAR_cluster_name}
 terraform plan -destroy -var-file=${BRANCH_NAME}.tfvars
 terraform destroy -var-file=${BRANCH_NAME}.tfvars -auto-approve
