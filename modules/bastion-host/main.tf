c4ec1c2ccc99fcc2c4b7fda08eb5960dada41d6a
Wyrzutowicz <jwwz@gft.com>
2021-06-23 14:07:58 +0200

    updated instance vm module version

--- modules/bastion-host/main.tf
+++ modules/bastion-host/main.tf
@@ -1,7 +1,7 @@
 # A testing VM to allow OS Login + IAP tunneling.
 module "instance_template" {
   source               = "terraform-google-modules/vm/google//modules/instance_template"
-  version              = "1.1.0"
+  version              = "6.5.0"
   project_id           = var.project
   machine_type         = var.machine_type
   subnetwork           = var.subnetwork
