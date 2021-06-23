43174c182d78c2a2d3d36cea52d549f6e3511d08
snoogft <snoo@gft.com>
2021-06-23 18:24:57 +0200

    Getting rid of this stupid terraform.tfvars that is preventing our pipeline to work. Set TF_VAR_cluster_name and TF_VAR_zone back in run.sh

--- k8s-config/run.sh
+++ k8s-config/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -xe
 
+export TF_VAR_cluster_name=$1 #first argument in call for run.sh in cloudbuild-dev.yaml
+export TF_VAR_zone=$2 #second argument in call for run.sh in cloudbuild-dev.yaml
+
 terraform init -upgrade
 terraform workspace select ${TF_VAR_cluster_name}
 terraform plan -var-file=${BRANCH_NAME}.tfvars
