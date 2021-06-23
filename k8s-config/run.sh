f8ddf0346ac78b624cc06fcb5af5919d35dfe4cc
snoogft <snoo@gft.com>
2021-06-23 16:59:26 +0200

    Created workspaces dev-cluster-1, dev-cluster-2, dev-secondary-cluster-1, dev-secondary-cluster-2, prod-cluster-1, prod-cluster-2 and changed run.sh with selected workspace

--- k8s-config/run.sh
+++ k8s-config/run.sh
@@ -4,6 +4,6 @@ TF_VAR_cluster_name=$1 #first argument in call for run.sh in cloudbuild-dev.yaml
 TF_VAR_zone=$2 #second argument in call for run.sh in cloudbuild-dev.yaml
 
 terraform init -upgrade
-terraform workspace select ${BRANCH_NAME}
+terraform workspace select ${TF_VAR_cluster_name}
 terraform plan -var-file=${BRANCH_NAME}.tfvars
 terraform apply -var-file=${BRANCH_NAME}.tfvars -auto-approve
