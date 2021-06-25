564c5522697544a89db77913a3e01eefb8b078a0
snoogft <snoo@gft.com>
2021-06-25 09:37:14 +0200

    Added custom operator path and added config-management-operator.yaml

--- k8s-config/acm.tf
+++ k8s-config/acm.tf
@@ -5,7 +5,7 @@ module "acm" {
   cluster_name     = data.google_container_cluster.my_cluster.name
   location         = data.google_container_cluster.my_cluster.location
   cluster_endpoint = data.google_container_cluster.my_cluster.endpoint
-
-  sync_repo   = "https://github.com/Katmar-creator/boa-management-config"
-  sync_branch = "main"
+  operator_path    = "config-management-operator/config-management-operator.yaml"
+  sync_repo        = "https://github.com/Katmar-creator/boa-management-config"
+  sync_branch      = "main"
 }
