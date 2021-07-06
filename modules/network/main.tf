60b0ba00e2d95a7c00a143cd6fd8c0bf1f23cacb
Wyrzutowicz <jwwz@gft.com>
2021-07-06 16:37:17 +0200

    change network name in subnets from va... to module...

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -17,7 +17,7 @@ module "subnets" {
   version = "~> 3.2.2"
 
   project_id   = var.project_id
-  network_name = var.network_name
+  network_name = module.vpc.network_name
   depends_on   = [module.vpc]
 
   subnets = [
